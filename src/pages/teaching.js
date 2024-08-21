e77d0a4914d1ec33106058f42f9571d4bd21506a
chimiseanga <seang696@gmail.com>
2024-08-20 23:17:04 -0700

    Updated Teaching page

--- src/pages/teaching.js
+++ src/pages/teaching.js
@@ -10,17 +10,21 @@ const TeachingPage = () => {
             <p>Current and past courses I've taught.</p>
             <h2>Current Courses</h2>
             <ul>
-               <li>Math 54 LEC 001 - Linear Algebra and Differential Equations (DIS 107), Spring 2024</li>
+               <li><a href="https://math.berkeley.edu/~sgonzales/math1_fall2024" target="_blank" rel="noreferrer">Math 1 LEC 001 - Foundations of Lower Division Mathematics</a>, Fall 2024</li>
             </ul>
 
             <h2>Past Courses</h2>
             <ul>
-               <li><a href="https://math.berkeley.edu/~apaulin/Math%201%20(Fall%202023).html" target="_blank" rel="noreferrer">
-                     Math 1 LEC 001 - Fundamentals of Lower Division Mathematics
-               </a>, Fall 2023</li>
+               <li>Math 54 LEC 001 - Linear Algebra and Differential Equations (DIS 107), Spring 2024</li>
+               <li><a href="https://math.berkeley.edu/~apaulin/Math%201%20(Fall%202023).html" target="_blank"
+                      rel="noreferrer">
+                  Math 1 LEC 001 - Foundations of Lower Division Mathematics
+               </a>, Fall 2023
+               </li>
                <li><a href="https://math.berkeley.edu/~sgonzales/pce.html" target="_blank" rel="noreferrer">
                   Pre-Calculus Essentials Workshop
-               </a>, Spring 2023</li>
+               </a>, Spring 2023
+               </li>
                <li>Math 55 LEC 001 - Discrete Mathematics (DIS 104, 105), Fall 2022</li>
                <li>Math 1A LEC 002 - Calculus (DIS 202, 205), Fall 2021</li>
                <li>Math N1A LEC 003 - Calculus, Summer 2021</li>
